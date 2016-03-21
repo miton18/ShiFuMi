deb1a4281735d90c10b292460a3f6534563da18d
miton18 <rcd18@hotmail.fr>
2016-03-21 16:21:18 +0100

    client ok

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -4,6 +4,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.net.InetAddress;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import socket.CSocket;
 import socket.JSONObject;
 
@@ -15,6 +18,9 @@ public class Controller implements ActionListener {
    // le choix de l'ordinateur
    // "papier", "pierre" ou "ciseaux"
    private String choix;
+   private String serverChoice;
+   private int hasWin;
+   private int jeuJoueur;
    
    /* 
    Toute l'intelligence du jeu est dans le tableau suivant
@@ -24,11 +30,11 @@ public class Controller implements ActionListener {
      Les indices des colonnes correspondent aux choix de l'ordinateur,
       respectivement : 0 pour papier, 1 pour pierre, 2 pour ciseaux
    */      
-   private int gains[][] = {
+   /*private int gains[][] = {
       { 0,  1, -1},
       {-1,  0,  1},      
       { 1, -1,  0}
-   };
+   };*/
    
    // le constructeur
    public Controller() {
@@ -59,22 +65,31 @@ public class Controller implements ActionListener {
       }
       // Le traitement des choix joueur et ordinateur...
       else {
+
+
+
+
+
          // Choix aléatoire coté ordinateur
-         this.choix = choixAleatoire();
+         //this.choix = choixAleatoire();
+
          // Modification dans l'interface selon les choix du joueur
          switch (this.commande) {
             case "Papier":
-               jeuJoueur = 0;
+               this.jeuJoueur = 0;
                Interface.maReponse.setIcon(Interface.iconePapier);
                break;
             case "Pierre":
-               jeuJoueur = 1;
+               this.jeuJoueur = 1;
                Interface.maReponse.setIcon(Interface.iconePierre);
                break;
             case "Ciseaux":
-               jeuJoueur = 2;
+               this.jeuJoueur = 2;
                Interface.maReponse.setIcon(Interface.iconeCiseaux);      
          }
+
+         choixServeur();
+
          // Modification dans l'interface selon les choix de l'ordinateur
          switch (this.choix) {            
             case "Papier":
@@ -89,15 +104,17 @@ public class Controller implements ActionListener {
                jeuOrdi = 2;
                Interface.ordiReponse.setIcon(Interface.iconeCiseaux);      
          }
+
          // Traitement du résultat
          // Joueur gagne
-         if (this.gains[jeuJoueur][jeuOrdi]>0) {
+         if( this.hasWin == 1  ) {
+         //if (this.gains[this.jeuJoueur][jeuOrdi]>0) {
             Interface.gainJoueur++;
             Interface.monScore.setText(Interface.startHtml+new Integer(Interface.gainJoueur).toString()+Interface.endHtml);
             Interface.maFenetre.getContentPane().setBackground (Color.GREEN);
          }
          // Joueur perd
-         else if (this.gains[jeuJoueur][jeuOrdi]<0) {
+         else if ( this.hasWin == -1 ) {
             Interface.gainOrdi++;
             Interface.ordiScore.setText(Interface.startHtml+new Integer(Interface.gainOrdi).toString()+Interface.endHtml);         
             Interface.maFenetre.getContentPane().setBackground (Color.RED);
@@ -135,18 +152,22 @@ public class Controller implements ActionListener {
       return(choix);
    }
 
-   public boolean getResponse( String choixJoueur ) {
+   public void choixServeur() {
 
       try {
          CSocket socket = new CSocket( InetAddress.getByName("127.0.0.1"), 443 );
 
-         JSONObject req = JSONObject();
+         JSONObject req = new JSONObject();
+         req.put("choix", this.jeuJoueur);
 
-         JSONObject res = socket.emit("It's your turn", "");
+         JSONObject res = socket.emit("It's your turn", req);
+
+         this.serverChoice = res.getString("server");
+         this.hasWin       = Integer.parseInt( res.getString("win") );
       }
       catch (Exception e) {
 
-         System.out.println("");
+         Logger.getGlobal().log( Level.SEVERE, e.getMessage() );
       }
    }
 
